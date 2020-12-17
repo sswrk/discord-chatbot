12de4e287e10c4165e8dc2dc2f04ef38e6a1256e
Szymon Świerk <swierk.szymon99@gmail.com>
2020-12-17 01:23:40 +0100

    done

--- intents/covid.py
+++ intents/covid.py
@@ -7,7 +7,8 @@ def try_covid(message):
             message.content.find("coronavirus") != -1 or
             (message.content.find("koron") != -1 and message.content.find("wirus") != -1) or
             message.content.find("pandem") != -1 or
-            message.content.find("epidem") != -1):
+            message.content.find("epidem") != -1 or
+            message.content.find("covid") != -1):
         cases = json.loads(covid.get_country_cases("Poland").__str__().replace('\'', '\"'))['NewCases']
         return f"Smuci mnie ten koronawirus, nowe przypadki w Polsce:\n{cases}"
 
